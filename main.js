a393e40dcf192cde857b419b4a5416e8ee4b3413
penmate90 <84581597+penmate90@users.noreply.github.com>
2021-10-07 11:40:35 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var width = screen.width;
 var new_width= screen.width - 70;
-var new_height= screen.height - 460;
+var new_height= screen.height - 360;
 var last_position_of_x, last_position_of_y;
 
     canvas = document.getElementById('myCanvas');
